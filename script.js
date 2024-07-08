ee9e6fa7164c49f055d586c07f565c47c8d46790
Icaro Jordano <Icarojordano>
2024-07-08 20:40:34 -0300

    ChamarEAdd

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const input=document.querySelector('.input')
 const div=document.querySelector('.lista')
-const lista=[1,2,3,4,5,6,7]
-let itens=``
-lista.map((e)=>{itens+=`<li>${e}</li>`})
+
+
 input.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    if (!itens.includes(`${input.value}`)){        itens+=`<li>${input.value}</li>`}
-
+    let itens=``
+    let listaAuxiliar=[]
+    // console.log(typeof(localStorage.getItem("LISTA")))
+    let lista=JSON.parse(localStorage.getItem("LISTA"))
+    listaAuxiliar=[...lista]
+    lista.map((e)=>{itens+=`<li>${e}</li>`;})
+    // .map((e)=>{itens+=`<li>${e}</li>`})
+    if (!itens.includes(`${input.value}`)){
+        itens+=`<li>${input.value}</li>`;
+        listaAuxiliar.push(input.value)}
     div.innerHTML=itens
+    localStorage.setItem("LISTA",JSON.stringify(listaAuxiliar))
     }
 });
